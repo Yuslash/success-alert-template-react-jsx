bb363ec0e58e5874335301f7519eff6ab5e91d2e
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-08-31 18:20:40 +0530

    added onclick event to redirect to page

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -1,9 +1,14 @@
 // import React from 'react'
 
 const HomeButton = () => {
+
+  const handleClick = () => {
+    window.location.href = 'http://localhost:5173';
+  }
+
   return (
     <div className="mt-10 flex justify-center p-4 ">
-          <button className="text-white border rounded-lg border-gray-600 py-4 px-8">Return to Homepage</button>
+          <button onClick={handleClick} className="text-white border rounded-lg border-gray-600 py-4 px-8">Return to Homepage</button>
     </div>
   )
 }
